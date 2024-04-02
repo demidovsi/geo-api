79aa2b9a072ec5a1582daf66c575ad04820123b2
demidovsi <104678541+demidovsi@users.noreply.github.com>
2024-04-02 10:51:02 +0300

    - рефакторинг

--- v1_usa.py
+++ v1_usa.py
@@ -26,6 +26,7 @@ def v1_usa_his_cities_get(city_id=None):
         for data in answer_metrics:
             metrics.append({"metric": data['param_name'], "source": data['sh_name'], "metric_name": data['name'],
                             "url": data['code']})
+
     url = "v1/select/{schema}/v_nsi_cities?where=name_country='United States'".format(schema=config.SCHEMA_NAME)
     answer_cities, is_ok, status = send_rest(url)
     if is_ok:
@@ -34,7 +35,8 @@ def v1_usa_his_cities_get(city_id=None):
         for data in answer_cities:
             st_id = st_id + ', ' if st_id else st_id
             st_id += str(data['id'])
-            cities.append({"city_id": data['id'], "city_name": data["sh_name"], "country_name": data['name_country']})
+            cities.append({"city_id": data['id'], "city_name": data["sh_name"], "country_name": data['name_country'],
+                           "population": data['population']})
         txt, is_ok, token_admin, lang_admin = common.login_superadmin()
         st = "select *, 'life_index' as param_name from {schema}.his_cities_life_index " \
              "where cities_id in ({st_id})" \
@@ -45,9 +47,7 @@ def v1_usa_his_cities_get(city_id=None):
             st_id=st_id, schema=config.SCHEMA_NAME
         )
         answer, is_ok, status = send_rest('v1/execute?view=1', 'PUT', params=st, token_user=token_admin)
-    # return json.dump(result, indent=4, ensure_ascii=False, sort_keys=True)
     answer = json.loads(answer)
-    result = list()
     for data in answer:
         city_id = data['0']
         result.append({'city_id': city_id, 'date': data['1'], 'value': data['2'], 'metric': data['3']})
